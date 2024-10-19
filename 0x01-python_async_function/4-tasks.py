7e6a2fe47fab9043af179c11d32eaa8216a819ca
Victory Chibuike <victorychibuike121@gmail.com>
2024-10-19 04:55:09 +0100

    Moved to correct folder

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -11,6 +11,7 @@ from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Return a list of delays in ascending order"""
     delays = [task_wait_random(max_delay) for _ in range(n)]
