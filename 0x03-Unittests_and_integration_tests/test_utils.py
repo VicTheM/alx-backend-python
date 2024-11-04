e6af9dc947d548150bd2cc87c2af8092052652f8
Victory Chibuike <victorychibuike121@gmail.com>
2024-11-04 20:47:36 +0100

    Added test for exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,3 +21,13 @@ class TestAccessNestedMap(unittest.TestCase):
             ):
         """Test case for a function that accesses nested map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ["a"]),
+        ({"a": 1}, ["a", "b"])
+        ])
+    def test_access_nested_map_exception(
+            self, nested_map: Mapping, path: Sequence):
+        """Test that an exception is raised"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
